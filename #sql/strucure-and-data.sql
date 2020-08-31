a6501920cb8c6b6c5389bcd4945f83eb4014773d
Artem Chornovol <artem.chornovol.r@gmail.com>
2020-08-31 16:34:11 +0300

    sql dump

---
+++ #sql/strucure-and-data.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v12.2.6 (64 bit)
+MySQL - 10.4.11-MariaDB : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `test`;
+
+/*Table structure for table `currency` */
+
+DROP TABLE IF EXISTS `currency`;
+
+CREATE TABLE `currency` (
+  `currencyId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `valuteId` varchar(50) COLLATE utf8_bin NOT NULL,
+  `numCode` int(11) NOT NULL,
+  `charCode` varchar(100) COLLATE utf8_bin NOT NULL,
+  `name` varchar(200) COLLATE utf8_bin NOT NULL,
+  `value` varchar(50) COLLATE utf8_bin NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  KEY `currencyId` (`currencyId`),
+  FULLTEXT KEY `search` (`valuteId`)
+) ENGINE=InnoDB AUTO_INCREMENT=9351 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `currency` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
